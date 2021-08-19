9a026ba10e0b786d7455f1bf5476d5c20976e2a7
Serin <bcode429@gmail.com>
2021-08-19 21:04:42 +0900

    [feat] urlencoded

--- web/config/express.js
+++ web/config/express.js
@@ -5,6 +5,7 @@ module.exports = function () {
     const app = express();
 
     app.use(express.json());
+    app.use(express.urlencoded({extended: true}));
 
     app.set("view engine", "ejs");
     app.set("views", process.cwd() + "/views");
